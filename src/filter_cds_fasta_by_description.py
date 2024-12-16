e3c2a24e90f43925388fdc595efb0c831c040e55
Khushboo Bhatia <khushboo.bhatia22@gmail.com>
2024-12-16 02:57:57 -0500

    Adding filter_cds_fasta_by_description.py

---
+++ src/filter_cds_fasta_by_description.py
@@ -0,0 +1,17 @@
+from Bio import SeqIO
+
+# Specify the path to your FASTA file
+fasta_files = ['B.1.1.529_cds.fna', 'B.1.351_cds.fna',	'B.1.525_cds.fna', 'B.1.617.1_cds.fna',	'C.37_cds.fna',	'P.2_cds.fna', 'B.1.1.7_cds.fna', 'B.1.427_cds.fna', 'B.1.526_cds.fna',	'B.1.617.2_cds.fna', 'P.1_cds.fna',	'P.3_cds.fna', 'JN.1.11.1_cds.fna', 'KP.3.1.1_cds.fna', 'XBB.1.5_cds.fna', 'XEC_cds.fna']
+path = '/Volumes/Extreme SSD/project/ncbi/genomes/new/'
+
+result = []
+for fasta_file in fasta_files:
+    # Parse the FASTA file and iterate through the sequences
+    for record in SeqIO.parse(path+fasta_file, "fasta"):
+        if 'surface glycoprotein' in record.description :
+            result.append(record)
+
+    outfile = path+fasta_file+'_filtered'
+    with open(outfile, 'w') as outhandle:
+        SeqIO.write(result, outhandle, "fasta")
+
